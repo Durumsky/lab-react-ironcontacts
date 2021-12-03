3c9798085c8a79cc3b2fa3bbc75b9e7777d0e882
Jones Durumsky <jonesdurumsky@gmail.com>
2021-12-03 14:41:29 +0100

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -8,28 +8,37 @@ function App() {
 
   return (
     <div className="App">
-    <h1>IronContacts</h1>
-    <div class="table-wrapper">
-      <table>
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-        </thead>
-        {fiveContacts.map((contact) => {
-          return (
-            <tbody>
-              <td>
-              <img src={contact.pictureUrl}  alt="contact" style={{height: 100}}/>
-              {contact.firstName}</td>
-              <td>{contact.name}</td>
-              <td>{contact.popularity}</td>    
-            </tbody>
-          );
-        })}
-      </table>
+      <h1>IronContacts</h1>
+      <div class="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won Oscar</th>
+              <th>Won Emmy</th>
+            </tr>
+          </thead>
+          {fiveContacts.map((contact) => {
+            return (
+              <tbody>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    alt="contact"
+                    style={{ height: 100 }}
+                  />
+                  {contact.firstName}
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                {contact.wonOscar && <td>🏆</td>}
+                {contact.wonEmmy && <td>🏆</td>}
+              </tbody>
+            );
+          })}
+        </table>
       </div>
     </div>
   );
